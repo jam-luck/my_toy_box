44e70257104bac072dd820f4eb6f701fe3a284f0
JAEMOON YOON <jaemoonyoon@JAEMOONui-Macmini.local>
2022-06-19 20:26:12 +0900

    material-ui 적용

--- frontend/src/components/PageTop.js
+++ frontend/src/components/PageTop.js
@@ -1,4 +1,7 @@
 import React, {useState,useEffect} from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 const PageTop = () => {
     const [connect, setConnect] = useState("연결안됨"); 
     const [userName, setUserName] = useState("사용자 정보 없음"); 
@@ -10,12 +13,18 @@ const PageTop = () => {
             });
     },[])
     return (
-        <div>
-            사용자정보창 <br/>
-            
-            <p>서버 연결상태: {connect}</p>
-            <p>유저정보: {userName}</p>
-        </div>
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    사용자정보창
+                </Typography>
+                <Typography variant="body2">
+                    서버 연결상태: {connect}
+                    <br />
+                    유저정보: {userName}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 }
 export default PageTop;
