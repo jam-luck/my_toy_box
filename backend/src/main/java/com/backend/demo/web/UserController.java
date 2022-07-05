2fd87084162d9c9a8ea36f15e23863e43dda157a
JAEMOON YOON <jaemoonyoon@JAEMOONui-Macmini.local>
2022-07-05 22:30:01 +0900

    swagger 3.0 추가, jpa 테스트

---
+++ backend/src/main/java/com/backend/demo/web/UserController.java
@@ -0,0 +1,31 @@
+package com.backend.demo.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.demo.domain.UserEntity;
+import com.backend.demo.domain.UserRepository;
+@RestController
+public class UserController {
+	@Autowired
+	UserRepository userRepository;
+	@GetMapping("/user")
+	public List<UserEntity> showUser() {
+		return userRepository.findAll().stream().toList();
+	}
+	/*
+	@GetMapping(value="/user/{name}")
+	public void addUser() {
+		UserEntity user = UserEntity.builder()
+				.name("test")
+				.userId("test")
+				.userPwd("test")
+				.type("user")
+				.build();
+		userRepository.save(user);
+	}
+	*/
+}
--- frontend/src/components/PageTop.js
+++ frontend/src/components/PageTop.js
@@ -3,10 +3,11 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 const PageTop = () => {
-    const [connect, setConnect] = useState("연결안됨");
+    //const [connect, setConnect] = useState("연결안됨");
     const [Spring3, setSpring3] = useState("");
-    const [userName, setUserName] = useState("사용자 정보 없음");
+    //const [userName, setUserName] = useState("사용자 정보 없음");
     const [userList, setUserList] = useState([]);  
+    /*
     useEffect(()=> {
         fetch('/ping')
             .then(response => response.text())
@@ -14,6 +15,7 @@ const PageTop = () => {
                 setConnect(msg);
             });
     },[])
+    */
     useEffect(()=> {
         fetch('/spring3/test')
             .then(response => response.text())
@@ -35,10 +37,6 @@ const PageTop = () => {
                     사용자정보창
                 </Typography>
                 <Typography variant="body2">
-                    서버 연결상태: {connect}
-                    <br />
-                    유저정보: {userName}
-                    <br />
                     Spring3 Rest: {Spring3}
                     <br/>
                     userList: {userList}
