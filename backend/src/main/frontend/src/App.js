43b6153a2f0655162c803c36e9a8af92ba4d7d81
JAEMOON YOON <jaemoonyoon@JAEMOONui-Macmini.local>
2022-07-07 23:28:07 +0900

    빌드테스트

--- backend/src/main/frontend/src/App.js
+++ backend/src/main/frontend/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import PageTop from './components/PageTop';
-import PageMain from './components/PageMain';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <div>
-        <PageTop/>
-        <PageMain/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
